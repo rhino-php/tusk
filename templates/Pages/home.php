43733b236e13c78edb44b14acf4898e258708d50
Carsten Coull <carsten.coull@swu.de>
2023-02-27 15:58:42 +0100

    init

---
+++ templates/Pages/home.php
@@ -0,0 +1,6 @@
+<h1>Test</h1>
+<?= $foo ?><br>
+<?= $page ?><br>
+<?= $subpage ?><br>
+
+<?= $this->Html->link("Some Test", ["customers", "new"]) ?>
---
+++ templates/element/partials/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer box">
+	<div class="outer-bound inverted">
+		<div class="stack">
+			Some Text
+			<!-- $this->getWidgetChannel(1) ?> -->
+		</div>
+
+		<!-- $this->parsePHP(PATHTOWEBROOT . 'templates/shapes/components/nav.php', [
+			'navId' => 'footer-nav',
+			'parentId' => 4
+		]); ?> -->
+	</div>
+</footer>
