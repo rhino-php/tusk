fb2d88fe9e7f598251974c8febf9ceb2ce1d63c6
Tyqo <carsten.coull@hotmail.de>
2023-07-16 15:54:33 +0200

    added Skeleton Folder

---
+++ Skeleton/templates/element/partials/main.php
@@ -0,0 +1,14 @@
+<div id="flash-messages" class="flash-messages">
+	<?= $this->Flash->render() ?>
+</div>
+
+<main id="main" class="main-content">	
+	<?= $this->fetch('content') ?>
+
+	<!-- Dummy Element for appling margin to -->
+	<hr class="footer-margin" />
+</main>
+
+<div id="overlay" class="overlay">
+	<!-- $this->parsePHP(PATHTOWEBROOT . 'templates/shapes/components/light-box.php') ?> -->
+</div>
---
+++ Skeleton/templates/element/text.php
@@ -0,0 +1,3 @@
+<div class="inner-bound">
+	<div><?= $this->parseEditor($html) ?></div>
+</div>
