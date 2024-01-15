1e0fae1f59becf930a7b661a138638fdc341b5c8
Carsten Coull <carsten.coull@swu.de>
2024-01-15 17:20:51 +0100

    added splitt template

---
+++ templates/element/horizontal-split.php
@@ -0,0 +1,9 @@
+<div class="inner-bound grid">
+	<section>
+		<?= $this->Rhino->slot(parentId: $id) ?>
+	</section>
+
+	<section>
+		<?= $this->Rhino->slot(parentId: $id) ?>
+	</section>
+</div>
---
+++ templates/element/vertical-split.php
@@ -0,0 +1,9 @@
+<div class="inner-bound stack">
+	<section>
+		<?= $this->Rhino->slot(parentId: $id) ?>
+	</section>
+
+	<section>
+		<?= $this->Rhino->slot(parentId: $id) ?>
+	</section>
+</div>
